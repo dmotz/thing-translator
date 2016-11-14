23392d2be3e07ce144e4edfa0c358ee38a8e4d6a
Dan Motzenbecker <dan@oxism.com>
2016-11-13 23:25:57 -0500

    main view

---
+++ src/views/main-view.js
@@ -0,0 +1,46 @@
+import html from 'choo/html'
+
+export default (state, prev, send) =>
+  html`
+    <section id="main-view" class=${state.isSnapping ? 'faded' : ''}>
+      ${
+        !state.firstTime && state.translation
+        ? html`
+          <div class="row">
+            <h2>${state.translation}</h2>
+            <h4 onclick=${_ => send('showList')}>
+              ${state.activeLang}
+            </h4>
+          </div>
+        `
+        : null
+      }
+      ${
+        !state.firstTime && state.label
+        ? html`
+          <div class="row">
+            <h2>${state.label}</h2>
+            <h4>${state.targetLang}</h4>
+          </div>
+        `
+        : null
+      }
+      ${
+        state.cameraReady
+        ? html`
+          <div
+            id="shutter-button"
+            class="${state.isSnapping ? 'busy' : ''}"
+            onclick=${_ => send('snap')}>
+          </div>
+        `
+        : null
+      }
+      ${
+        state.firstTime
+        ? html`<h5 id="first-time">Try taking a picture of something.</h5>`
+        : null
+      }
+      <div class="debug">${state.guesses}</div>
+    </section>
+  `
