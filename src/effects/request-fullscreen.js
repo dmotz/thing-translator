c4c5cde509f9603670f9c25563ab85303f047f98
Dan Motzenbecker <dan@oxism.com>
2016-11-13 23:25:09 -0500

    request fullscreen module

---
+++ src/effects/request-fullscreen.js
@@ -0,0 +1,12 @@
+export default function requestFullscreen(_, state, send, done) {
+  if (document.body.requestFullscreen) {
+    document.body.requestFullscreen()
+  } else if (document.body.webkitRequestFullscreen) {
+    document.body.webkitRequestFullscreen()
+  } else if (document.body.mozRequestFullScreen) {
+    document.body.mozRequestFullScreen()
+  } else if (document.body.msRequestFullscreen) {
+    document.body.msRequestFullscreen()
+  }
+  send('setFullscreen', done)
+}
