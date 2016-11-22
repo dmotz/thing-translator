df95fb3f7729b27e607c1defe3fef6fce56689c5
Dan Motzenbecker <dan@oxism.com>
2016-11-22 13:28:46 -0500

    when using enumerateDevices, make 2 attempts to find environment camera

--- src/effects/request-camera.js
+++ src/effects/request-camera.js
@@ -1,7 +1,102 @@
+const sourceEnumSupport  = navigator.mediaDevices && navigator.mediaDevices.enumerateDevices
+const streamTrackSupport = MediaStreamTrack && MediaStreamTrack.getSources
+const sourceSupport      = sourceEnumSupport || streamTrackSupport
+
+let attemptedTwice = false
+let lastStream
+
 const getUserMedia = (() => {
   const fn = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia
   return fn ? fn.bind(navigator) : null
 })()
+
+
+const findBestSource = sources => {
+  let source = null
+
+  if (sourceSupport && sources && sources.length) {
+
+    if (sourceEnumSupport) {
+      for (let i = 0; i < sources.length; i++) {
+        const candidate = sources[i]
+
+        if (candidate.kind === 'videoinput') {
+          if (typeof candidate.getCapabilities === 'function') {
+            const capabilities = candidate.getCapabilities()
+
+            if (capabilities && capabilities.facingMode === 'environment') {
+              source = candidate
+              break
+            }
+          }
+
+          if (/facing back/i.test(candidate.label)) {
+            source = candidate
+            break
+          }
+        }
+      }
+    } else {
+      source = sources.find(s => s.facing === 'environment')
+      if (!source) {
+        source = sources.find(s => s.kind === 'video')
+      }
+    }
+  }
+
+  return source
+}
+
+
+const activateCamera = (sources, send, done) => {
+  const source = findBestSource(sources)
+
+  getUserMedia(
+    {
+      audio: false,
+      video: source
+        ? {optional: [{sourceId: sourceEnumSupport ? source.deviceId : source.id}]}
+        : true
+    },
+    stream => {
+      lastStream = stream
+
+      if (sourceEnumSupport && !source && !attemptedTwice) {
+        attemptedTwice = true
+        setTimeout(() => {
+          stream.getTracks().forEach(track => track.stop())
+          enumerateDevices(send, done)
+        }, 1)
+        return
+      }
+
+      const canvas    = document.getElementById('canvas')
+      const videoEl   = document.getElementById('video')
+      const streamUrl = URL.createObjectURL(stream)
+
+      videoEl.src = streamUrl
+
+      send(
+        'setStream',
+        {
+          stream: streamUrl,
+          video:  videoEl,
+          ctx:    canvas.getContext('2d'),
+          canvas
+        },
+        done
+      )
+    },
+    send.bind(null, 'cameraError', done)
+  )
+}
+
+
+const enumerateDevices = (send, done) =>
+  navigator.mediaDevices.enumerateDevices()
+    .then(sources => activateCamera(sources, send, done))
+    .catch(_ => activateCamera(null, send, done))
+
 
 export default function requestCamera(_, state, send, done) {
   if (state.cameraReady) {
@@ -12,79 +107,11 @@ export default function requestCamera(_, state, send, done) {
     return send('cameraError', done)
   }
 
-  const sourceEnumSupport  = navigator.mediaDevices && navigator.mediaDevices.enumerateDevices
-  const streamTrackSupport = MediaStreamTrack && MediaStreamTrack.getSources
-
-  const initCameraRequest = sources => {
-    let source
-    const sourceSupport = sourceEnumSupport || streamTrackSupport
-
-    if (sourceSupport && sources && sources.length) {
-
-      if (sourceEnumSupport) {
-        for (let i = 0; i < sources.length; i++) {
-          const candidate = sources[i]
-
-          if (candidate.kind === 'videoinput') {
-            if (typeof candidate.getCapabilities === 'function') {
-              const capabilities = candidate.getCapabilities()
-
-              if (capabilities && capabilities.facingMode === 'environment') {
-                source = candidate
-                break
-              }
-            }
-
-            if (/facing back/i.test(candidate.label)) {
-              source = candidate
-              break
-            }
-          }
-        }
-      } else {
-        source = sources.find(s => s.facing === 'environment')
-        if (!source) {
-          source = sources.find(s => s.kind === 'video')
-        }
-      }
-    }
-
-    getUserMedia(
-      {
-        audio: false,
-        video: source
-          ? {optional: [{sourceId: sourceEnumSupport ? source.deviceId : source.id}]}
-          : true
-      },
-      stream => {
-        const canvas    = document.getElementById('canvas')
-        const videoEl   = document.getElementById('video')
-        const streamUrl = URL.createObjectURL(stream)
-
-        videoEl.src = streamUrl
-
-        send(
-          'setStream',
-          {
-            stream: streamUrl,
-            video:  videoEl,
-            ctx:    canvas.getContext('2d'),
-            canvas
-          },
-          done
-        )
-      },
-      send.bind(null, 'cameraError', done)
-    )
-  }
-
   if (sourceEnumSupport) {
-    navigator.mediaDevices.enumerateDevices()
-      .then(initCameraRequest)
-      .catch(_ => initCameraRequest(null))
+    enumerateDevices(send, done)
   } else if (streamTrackSupport) {
-    MediaStreamTrack.getSources(initCameraRequest)
+    MediaStreamTrack.getSources(sources => activateCamera(sources, send, done))
   } else {
-    initCameraRequest(null)
+    activateCamera(null, send, done)
   }
 }
