183326aee60ea4c85b16f64e0ce4c3970f10991f
Alexey Rodionov <fluorescent.hallucinogen@gmail.com>
2016-11-25 11:46:37 +0300

    Make the web app progressive
    Learn more about Progressive Web Apps (PWA) at https://goo.gl/wJPiaj

---
+++ sw-precache-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  staticFileGlobs: [
+    'index.html',
+    'app.js',
+    'app.css',
+    'service-worker-registration.js',
+    'manifest.json',
+    'logo-192x192.png',
+    'logo-512x512.png',
+    'logo.svg'
+  ]
+};
